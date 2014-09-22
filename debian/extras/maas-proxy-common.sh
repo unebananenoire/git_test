d768eb725d54f5f39446a3330168411a8f2f2e97
Andres Rodriguez <andreserl@ubuntu.com>
2014-09-22 08:32:39 +0000

    [r=andreserl,gmb][bug=][author=andreserl] Replace squid-deb-proxy; run our own proxy.  Places a proxy on port 8000 for backwards compatibility, but also provides a transparent proxy if iptables are configured for such case.
    This that can be done in MAAS is to generate a maas-proxy template that configures the proxy in different ways, including allowing only networks that MAAS manages, instead of all.

---
+++ debian/extras/maas-proxy-common.sh
@@ -0,0 +1,31 @@
+pre_start() {
+  if [ -x /usr/sbin/squid ]; then
+      SQUID=/usr/sbin/squid
+  elif  [ -x /usr/sbin/squid3 ]; then
+      SQUID=/usr/sbin/squid3
+  else
+      echo "No squid binary found"
+      exit 1
+  fi
+
+  # ensure all cache dirs are there
+  install -d -o proxy -g proxy -m 750 /var/cache/maas-proxy/
+  install -d -o proxy -g proxy -m 750 /var/log/maas/proxy/
+  install -m 750 -o proxy -g proxy -d /var/spool/maas-proxy/
+  if [ ! -d /var/cache/maas-proxy/00 ]; then
+   $SQUID -z -N -f /etc/maas/maas-proxy.conf
+  fi
+}
+
+# from the squid3 debian init script
+find_cache_dir () {
+        w="     " # space tab
+        res=`sed -ne '
+                s/^'$1'['"$w"']\+[^'"$w"']\+['"$w"']\+\([^'"$w"']\+\).*$/\1/p;
+                t end;
+                d;
+                :end q' < $CONFIG`
+        [ -n "$res" ] || res=$2
+        echo "$res"
+}
+
