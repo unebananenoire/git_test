c1d47abfc2abf980e25d0fda1f87a7eaf3872ddc
Andres Rodriguez <andreserl@ubuntu.com>
2014-11-14 14:10:32 +0000

    [r=andreserl][bug=][author=andreserl] Update trunk packaging to reflect the recent changes for 1.7

--- debian/extras/maas-proxy-common.sh
+++ debian/extras/maas-proxy-common.sh
@@ -12,6 +12,9 @@ pre_start() {
   install -d -o proxy -g proxy -m 750 /var/cache/maas-proxy/
   install -d -o proxy -g proxy -m 750 /var/log/maas/proxy/
   install -m 750 -o proxy -g proxy -d /var/spool/maas-proxy/
+  if [ -d /var/log/maas/proxy ]; then
+   chown -R proxy:proxy /var/log/maas/proxy
+  fi
   if [ ! -d /var/cache/maas-proxy/00 ]; then
    $SQUID -z -N -f /etc/maas/maas-proxy.conf
   fi
